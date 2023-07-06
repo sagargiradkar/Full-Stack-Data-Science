d5a00804731dc7350ac488290b0ba717551069ee
sagargiradkar <spgiradkar2002@gmail.com>
2023-07-07 01:32:19 +0530

    print function added

---
+++ print.py
@@ -0,0 +1,17 @@
+a=10
+b=20
+c=a+b
+print("Addition :",c)
+
+print("Value of a and b is ",a,b,c,sep=":",end="........")
+#formated string
+
+#   print(data,seperatoe,end)  %s,%d,%i,%f
+
+a=int(input("Enter a first Number :"))
+b=int(input("Enter Second number :"))
+c=a+b
+
+print("Addition of %d and %d is %d "%(a,b,c))
+
+print("Addition of %f and %f is %f "%(a,b,c))
